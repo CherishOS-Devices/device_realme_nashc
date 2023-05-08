46cb5a3858ab655764cf02db53edb0c5be37647a
venudinesh <venudinesh21@gmail.com>
2023-05-08 20:51:06 +0530

    It's cherish time

--- cherish_nashc.mk
+++ cherish_nashc.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/realme/nashc/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common CherishOS stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_nashc
+PRODUCT_NAME := cherish_nashc
 PRODUCT_DEVICE := nashc
 PRODUCT_MANUFACTURER := Realme
 PRODUCT_BRAND := Realme
@@ -29,3 +29,15 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="RMX3085-user 12 SP1A.210812.016 R.GDPR.c3619e_1 release-keys"
 
 BUILD_FINGERPRINT := realme/RMX3085/RMX3085L1:12/SP1A.210812.016/R.GDPR.c3619e_1:user/release-keys
+
+# FOD animations
+EXTRA_UDFPS_ANIMATIONS := true
+
+# Cherish Unofficial 
+CHERISH_BUILD_TYPE := OFFICIAL
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+ro.cherish.maintainer=Eren Yeager
+
+# MISC stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+ALLOW_MISSING_DEPENDENCIES=true
